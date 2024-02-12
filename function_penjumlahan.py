8bf7664b5785fff802179b69b23c48febc3e2933
afaafa <afhazachra@gmail.com>
2024-02-12 10:59:38 +0700

    tambah docstring

--- function_penjumlahan.py
+++ function_penjumlahan.py
@@ -1,4 +1,13 @@
 def penjumlahan(angka_1, angka_2):
+    """
+    Membuat function 
+    Args:
+        angka_1 (_type_): _description_
+        angka_2 (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     jumlah = angka_1 + angka_2
     return jumlah
-penjumlahan(1,2)
+penjumlahan(9,10)
